a5af284a90a4cf1507c2445f07a8a1ebda1b0767
Soha Jin <soha@lohu.info>
2018-03-06 21:30:04 +0800

    [not finished] Command for turing with gyro

--- src/org/usfirst/frc/team5453/robot/commands/TurnWithGyroCommand.java
+++ src/org/usfirst/frc/team5453/robot/commands/TurnWithGyroCommand.java
@@ -1,36 +1,49 @@
 package org.usfirst.frc.team5453.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team5453.robot.Robot;
 
-/**
- *
- */
-public class TurnWithGyroCommand extends Command {
+public class TurnWithGyroCommand extends Command{
+	double dstAngle=0;
+	double speedRate=0.6;
 
-    public TurnWithGyroCommand() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+	
+	// Here `angle`, positive is clockwise, negative is anticlockwise.
+	public TurnWithGyroCommand(double angle){
+		requires(Robot.drivingSys);
+		dstAngle=angle;
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	public TurnWithGyroCommand(double angle,double spdRate){
+		requires(Robot.drivingSys);
+		dstAngle=angle;
+		speedRate=spdRate;
+	}
+	
+	protected void initialize(){
+		System.out.println("TurnWithGyroCommand("+dstAngle+") is initialized.");
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+	protected void execute(){
+		Robot.drivingSys.tankDrive((dstAngle>=0?-1:1)*speedRate,(dstAngle>=0?-1:1)*speedRate,false);
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	protected boolean isFinished(){
+		// NOTICE
+		// Here is not finished yet.
+		// How to get yaw angle?
+		// Refer to GyroAHRSSubsystem
+		// is BoardYawAxis correct?
+		return true;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	protected void end(){
+		System.out.println("TurnWithGyroCommand("+dstAngle+") is ended.");
+		Robot.drivingSys.resetMotors();
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	protected void interrupted(){
+		System.out.println("TurnWithGyroCommand("+dstAngle+") is interrupted.");
+		Robot.drivingSys.resetMotors();
+	}
 }
