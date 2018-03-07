d9bdd0ac28bcd2aa8143a329a124652f0661d0d2
Soha Jin <soha@lohu.info>
2018-03-07 15:33:37 +0800

    upgrade autonomous.GoStraight to TimedGoStraightCommand

---
+++ src/org/usfirst/frc/team5453/robot/commands/TimedGoStraightCommand.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team5453.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team5453.robot.Robot;
+import org.usfirst.frc.team5453.robot.RobotMap;
+
+public class TimedGoStraightCommand extends Command{
+	double speed=0.5;
+
+	long stopTimestamp;
+	long timeToExecute=10000;
+
+	public TimedGoStraightCommand(double v,long time){
+		timeToExecute=time;
+		speed=v;
+	}
+
+	protected void initialize(){
+		System.out.println("TimedGoStraightCommand("+speed+","+timeToExecute+") is initialized.");
+		stopTimestamp=System.currentTimeMillis()+timeToExecute;
+	}
+
+	protected void execute(){
+		Robot.drivingSys.arcadeDrive(speed,0,RobotMap.drivingSquaredInput);
+	}
+
+	protected boolean isFinished(){
+		return stopTimestamp<=System.currentTimeMillis();
+	}
+
+	protected void end(){
+		System.out.println("TimedGoStraightCommand("+speed+","+timeToExecute+") is ended.");
+		Robot.drivingSys.resetMotors();
+	}
+
+	protected void interrupted(){
+		System.out.println("TimedGoStraightCommand("+speed+","+timeToExecute+") is interrupted.");
+		Robot.drivingSys.resetMotors();
+	}
+}
