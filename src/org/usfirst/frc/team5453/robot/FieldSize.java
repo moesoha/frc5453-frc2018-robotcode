b626dfe6089533e338d3610b2f162b8a6c0015d2
Soha Jin <soha@lohu.info>
2018-03-07 15:33:57 +0800

    update FieldSize

--- src/org/usfirst/frc/team5453/robot/FieldSize.java
+++ src/org/usfirst/frc/team5453/robot/FieldSize.java
@@ -1,13 +1,20 @@
 package org.usfirst.frc.team5453.robot;
 
 public class FieldSize{
+	// width is the side of the field's longer side
+	// height is the side of the field's shorter side
+
 	// ALL UNITS ARE INCH
 	public static double robotLength=32;
 	public static double robotWidth=28;
 	
 	public static double fieldLength=648;
 	public static double fieldWidth=324;
+	public static double portalWidth=48;
+	public static double switchWidth=153.5;
+	public static double switchLength=56;
 	
 	public static double fromStationToSwitchLength=168;
+	public static double fromLengthSideToSwitchLength=(fieldWidth-switchWidth)/2;
 	public static double fromStationToScaleLength=fieldLength/2;
 }
