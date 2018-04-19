76ce5a6f7bcd9e8dba44546b136d9fbd16cde912
Soha Jin <soha@lohu.info>
2018-04-19 11:07:42 -0500

    Fix bugs

--- src/org/usfirst/frc/team5453/robot/commands/autonomous/ShePiPushRtAngleSwitchTimed.java
+++ src/org/usfirst/frc/team5453/robot/commands/autonomous/ShePiPushRtAngleSwitchTimed.java
@@ -10,17 +10,18 @@ import org.usfirst.frc.team5453.robot.commands.TimedElevatorCommand;
 public class ShePiPushRtAngleSwitchTimed extends CommandGroup{
 	// Same-side SWITCH or SCALE only
 	public ShePiPushRtAngleSwitchTimed(boolean isLeft){
-		char colorLocationSecond=DriverStation.getInstance().getGameSpecificMessage().charAt(1);
-		if(colorLocationSecond==(isLeft?'L':'R')){
+		char colorLocationFirst=DriverStation.getInstance().getGameSpecificMessage().charAt(0);
+		if(colorLocationFirst==(isLeft?'L':'R')){
 			addSequential(new org.usfirst.frc.team5453.robot.commands.SleepCommand(500));
 			addSequential(new TimedElevatorCommand(0.5,3000));
-			addParallel(new TimedElevatorCommand(0.2,2000));
-			addSequential(new org.usfirst.frc.team5453.robot.commands.TimedArcadeGoCommand(0.7,(isLeft?-1:-1.07)*0.22,1800));
-			addSequential(new org.usfirst.frc.team5453.robot.commands.TimedArcadeGoCommand(0.5,(isLeft?1:-1.1)*0.5,2000));
-			addParallel(new org.usfirst.frc.team5453.robot.commands.TimedGoStraightCommand(0.4,3000));
+			addParallel(new TimedElevatorCommand(0.2,4800));
+			addSequential(new org.usfirst.frc.team5453.robot.commands.TimedArcadeGoCommand(0.75,(isLeft?-1:-1.07)*0.22,2000));
+			addSequential(new org.usfirst.frc.team5453.robot.commands.TimedArcadeGoCommand(0.5,(isLeft?1:-1.1)*0.6,900));
+			addParallel(new org.usfirst.frc.team5453.robot.commands.TimedGoStraightCommand(0.5,3000));
+			addSequential(new org.usfirst.frc.team5453.robot.commands.SleepCommand(500));
 			addSequential(new SetIntakeCommand(RobotMap.operatingIntakeSpeedAutoConstant*-1,false),1000);
 		}else{
-			addSequential(new ShePiPushTimed(isLeft));
+			addSequential(new org.usfirst.frc.team5453.robot.commands.TimedArcadeGoCommand(0.6,(isLeft?-1:1)*0.31,5000));
 		}
 	}
 }
